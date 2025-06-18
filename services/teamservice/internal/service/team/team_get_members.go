a9e2834eddfb878fc65537e865eb91b090157729
pavel <pavel.termhg@gmail.com>
2025-06-19 00:41:20 +0300

    feat(team): connect team service with user service

--- services/teamservice/internal/service/team/team_get_members.go
+++ services/teamservice/internal/service/team/team_get_members.go
@@ -2,6 +2,8 @@ package team
 
 import (
 	"context"
+	"fmt"
+	userv1 "github.com/cms-crs/protos/gen/go/user_service"
 	"userservice/internal/dto"
 )
 
@@ -14,14 +16,34 @@ func (service *Service) GetTeamMembers(ctx context.Context, ID string) ([]*dto.T
 		return nil, err
 	}
 
-	var teamMembersDTO []*dto.TeamMember
+	var userIDs []string
+	for _, member := range teamMembers {
+		userIDs = append(userIDs, member.UserID)
+	}
+
+	users, err := service.userClient.GetUsersByIds(ctx, userIDs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %w", err)
+	}
+
+	userMap := make(map[string]*userv1.User)
+	for _, user := range users {
+		userMap[user.Id] = user
+	}
+
+	var members []*dto.TeamMember
 	for _, teamMember := range teamMembers {
-		teamMembersDTO = append(teamMembersDTO, &dto.TeamMember{
+		_, exists := userMap[teamMember.UserID]
+		if !exists {
+			continue
+		}
+
+		members = append(members, &dto.TeamMember{
 			UserID: teamMember.UserID,
 			TeamID: teamMember.TeamID,
 			Role:   teamMember.Role,
 		})
 	}
 
-	return teamMembersDTO, nil
+	return members, nil
 }
--- services/teamservice/internal/service/team/user_update_role.go
+++ services/teamservice/internal/service/team/user_update_role.go
@@ -2,6 +2,7 @@ package team
 
 import (
 	"context"
+	"fmt"
 	"userservice/internal/dto"
 )
 
@@ -9,7 +10,12 @@ func (service *Service) UpdateUserRole(
 	ctx context.Context,
 	req *dto.UpdateUserRoleRequest,
 ) (*dto.TeamMember, error) {
-	err := service.teamRepository.UpdateUserRole(ctx, req)
+	_, err := service.userClient.GetUser(ctx, req.UserID)
+	if err != nil {
+		return nil, fmt.Errorf("user not found: %w", err)
+	}
+
+	err = service.teamRepository.UpdateUserRole(ctx, req)
 
 	if err != nil {
 		return nil, err
